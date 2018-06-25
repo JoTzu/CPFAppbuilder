c63e132f24af651bd59639e19aa9f454650b9d8a
JoTzu <ra92392@gmail.com>
2018-06-25 16:25:09 +0800

    test 43

--- water/main.js
+++ water/main.js
@@ -1,14 +1,8 @@
-var water, light, text, relay;
+var water, relay;
 var time = 0;
 var x = 0;
 var value = ['警戒', '注意', '安全'];
 var word = ['現在地下道積水嚴重，無法行駛，請改道', '現在地下道有些許積水，請小心慢行', '現在地下道無積水，可安心行駛'];
-/*var value1 = "警戒";
-var value2 = "注意";
-var value3 = "安全";
-var word1 = "現在地下道積水嚴重，無法行駛，請改道";
-var word2 = "現在地下道有些許積水，請小心慢行";
-var word3 = "現在地下道無積水，可安心行駛";*/
 
 setup();
 
@@ -16,7 +10,7 @@ cpf.request('["grove_rgblcd_clear"]');
 
 function loop() {
     water = cpf.get("d6");
-    relay = cpf.get("d5");
+    relay = cpf.get("d4");
     light = document.getElementById("lightValue").value;
     text = document.getElementById("word").value;
 
@@ -25,17 +19,13 @@ function loop() {
             console.log(time);
             if (x != 1) {
                 cpf.request('["grove_rgblcd_clear"]');
-                document.getElementById("lightValue").innerHTML = value[x - 1];
-                document.getElementById("word").innerHTML = word[x - 1];
-                //document.getElementById("lightValue").innerHTML = value1;
-                //document.getElementById("word").innerHTML = word1;
             }
             cpf.request('["grove_rgblcd_print", 2, 0,"Underground"]');
             cpf.request('["grove_rgblcd_print", 4, 1,"NO ENTRY."]');
             cpf.request('["grove_rgblcd_set_rgb", 255, 160, 122]');
             cpf.request('["grove_setColorRGB", 0, 255, 0, 0]');
 
-            //cpf.set("d5", d5);
+            cpf.set("d4", d4);
 
             if (((time - 5) % 3) == 1) {
                 cpf.SetSpeech("On", "cmn-Hant-TW", "現在地下道無法通行請駕駛改道", 0.4, 0.7);
@@ -46,10 +36,6 @@ function loop() {
         else if (water == 0 && time <= 5) {
             if (x != 2) {
                 cpf.request('["grove_rgblcd_clear"]');
-                document.getElementById("lightValue").innerHTML = value[x - 1];
-                document.getElementById("word").innerHTML = word[x - 1];
-                //document.getElementById("lightValue").innerHTML = value2;
-                //document.getElementById("word").innerHTML = word2;
             }
             cpf.request('["grove_rgblcd_print", 2, 0,"Underground"]');
             cpf.request('["grove_rgblcd_print", 0, 1,"Drive carefully."]');
@@ -61,10 +47,6 @@ function loop() {
         else if (water == 1) {
             if (x != 3) {
                 cpf.request('["grove_rgblcd_clear"]');
-                document.getElementById("lightValue").innerHTML = value[x - 1];
-                document.getElementById("word").innerHTML = word[x - 1];
-                //document.getElementById("lightValue").innerHTML = value3;
-                //document.getElementById("word").innerHTML = word3;
             }
             cpf.request('["grove_rgblcd_print", 2, 0,"Underground"]');
             cpf.request('["grove_rgblcd_print", 2, 1,"Road safely."]');
@@ -75,12 +57,15 @@ function loop() {
         }
         console.log(water);
 
+        document.getElementById("lightValue").innerHTML = value[x - 1];
+        document.getElementById("word").innerHTML = word[x - 1];
+
     } setTimeout(loop, 1000);
 }
 loop();
 
 function setup() {
     if (cpf) {
-        cpf.setPinMode('["resetPin"], ["grove_rgblcd_begin", 16, 2], ["grove_newChainableLED",7, 8, 1], ["setPinMode", "digital", 5, "INPUT"], ["setPinMode", "digital", 6, "INPUT"]');
+        cpf.setPinMode('["resetPin"], ["grove_rgblcd_begin", 16, 2], ["grove_newChainableLED",7, 8, 1], ["setPinMode", "digital", 4, "INPUT"], ["setPinMode", "digital", 6, "INPUT"]');
     }
 }
