8da639435d15a6c2c5e2a5eb201f5689c991e1d5
JoTzu <ra92392@gmail.com>
2018-06-26 16:08:14 +0800

    test 71

--- water/main.js
+++ water/main.js
@@ -7,7 +7,8 @@ var word = ['現在地下道積水嚴重，無法行駛，請改道', '現在地
 setup();
 
 cpf.request('["grove_setColorRGB", 0, 255, 0, 255]');
-cpf.request('["grove_rgblcd_clear"]');
+cpf.request('["grove_rgblcd_print", 2, 0,"Underground"]');
+cpf.request('["grove_rgblcd_print", 2, 1,"TEST."]');
 cpf.request('["digitalWrite", 4 , 0]');
 
 function loop() {
@@ -21,8 +22,8 @@ function loop() {
                 cpf.request('["grove_rgblcd_clear"]');
             }
 
-            // cpf.request('["grove_rgblcd_print", 2, 0,"Underground"]');
-            // cpf.request('["grove_rgblcd_print", 4, 1,"NO ENTRY."]');
+            cpf.request('["grove_rgblcd_print", 2, 0,"Underground"]');
+            cpf.request('["grove_rgblcd_print", 4, 1,"NO ENTRY."]');
             cpf.request('["grove_rgblcd_set_rgb", 255, 160, 122]');
             cpf.request('["grove_setColorRGB", 0, 255, 0, 0]');
 
@@ -41,8 +42,8 @@ function loop() {
                 cpf.request('["grove_rgblcd_clear"]');
             }
             //cpf.request('["digitalWrite", 4 , 0]');
-            // cpf.request('["grove_rgblcd_print", 2, 0,"Underground"]');
-            // cpf.request('["grove_rgblcd_print", 0, 1,"Drive carefully."]');
+            cpf.request('["grove_rgblcd_print", 2, 0,"Underground"]');
+            cpf.request('["grove_rgblcd_print", 0, 1,"Drive carefully."]');
             cpf.request('["grove_rgblcd_set_rgb", 238, 238, 0]');
             cpf.request('["grove_setColorRGB", 0, 255, 255, 0]');
             time += 1;
@@ -55,10 +56,9 @@ function loop() {
                 cpf.request('["grove_rgblcd_clear"]');
             }
 
-            // cpf.request('["grove_rgblcd_print", 2, 0,"Underground"]');
-            // cpf.request('["grove_rgblcd_print", 2, 1,"Road safely."]');
+            cpf.request('["grove_rgblcd_print", 2, 0,"Underground"]');
+            cpf.request('["grove_rgblcd_print", 2, 1,"Road safely."]');
             cpf.request('["grove_rgblcd_set_rgb", 124, 252, 0]');
-            console.log(77777);
             cpf.request('["grove_setColorRGB", 0, 0, 255, 0]');
             console.log('2  ' + time);
             if (x == 3) {
