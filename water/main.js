5ab6fe35ab4e1d2c433fea626d0f5f9ba5c4dcbc
JoTzu <ra92392@gmail.com>
2018-06-26 21:57:19 +0800

    test 94

--- water/main.js
+++ water/main.js
@@ -7,16 +7,15 @@ var word = ['現在地下道積水嚴重，無法行駛，請改道', '現在地
 setup();
 
 function loop() {
-    water = cpf.get("d6");   //  WaterSensor
+    water = cpf.get("d6");
 
     if (cpf) {
         if (water == 0) {
-            // 紅燈
             cpf.request('["grove_setColorRGB", 0, 255, 0, 0]');
             cpf.request('["digitalWrite", 4 , 1]');
+            cpf.SetSpeech("On", "cmn-Hant-TW", "現在地下道無法通行請駕駛改道", 0.4, 0.7);
         }
         else {
-            //綠燈
             cpf.request('["grove_setColorRGB", 0, 0, 255, 0]');
             cpf.request('["digitalWrite", 4 , 0]');
         }
@@ -34,68 +33,67 @@ loop();
 
 // function loop() {
 //     water = cpf.get("d6");   //  WaterSensor
-//     // cpf.request('["grove_setColorRGB", 0, 0, 0, 255]');
 
-//         if (water == 0 && time > 5) {  //  紅燈   WaterSensor == 0 有水
-//             console.log(time);
-//             if (x != 1) {          //  LCD板的變數
-//                 cpf.request('["grove_rgblcd_clear"]');
-//             }
-
-//             cpf.request('["grove_setColorRGB", 0, 255, 0, 0]');
-//             cpf.request('["grove_rgblcd_print", 2, 0,"WaterClean"]');
-//             cpf.request('["grove_rgblcd_print", 4, 1,"NO ENTRY."]');
-//             cpf.request('["grove_rgblcd_set_rgb", 255, 160, 122]');
-
-//             if (x == 1) {
-//                 cpf.request('["digitalWrite", 4 , 1]');
-//             }
-
-//             if (((time - 5) % 3) == 1) {
-//                 cpf.SetSpeech("On", "cmn-Hant-TW", "現在地下道無法通行請駕駛改道", 0.4, 0.7);
-//             }
-//             time += 1;
-//             x = 1;
-//         }
-//         else if (water == 0 && time <= 5) {   //  黃燈
-//             if (x != 2) {
-//                 cpf.request('["grove_rgblcd_clear"]');
-//             }
-//             cpf.request('["digitalWrite", 4 , 0]');
-//             cpf.request('["grove_setColorRGB", 0, 255, 255, 0]');
-//             cpf.request('["grove_rgblcd_print", 2, 0,"WaterClean"]');
-//             cpf.request('["grove_rgblcd_print", 0, 1,"Drive carefully."]');
-//             cpf.request('["grove_rgblcd_set_rgb", 238, 238, 0]');
-//             time += 1;
-//             x = 2;
-//         }
-//         else if (water == 1) {    //  綠燈    WaterSensor == 1 沒水
-//             console.log('1  ' + time);
-
-//             if (x != 3) {
-//                 cpf.request('["grove_rgblcd_clear"]');
-//             }
-
-//             cpf.request('["grove_setColorRGB", 0, 0, 255, 0]');
-//             //cpf.request('["grove_rgblcd_print", 2, 0,"Undergrand"]');
-//             //cpf.request('["grove_rgblcd_print", 2, 1,"Road safely."]');
-//             //cpf.request('["grove_rgblcd_set_rgb", 124, 252, 0]');
-
-//             console.log('2  ' + time);
-
-//             if (x == 3) {
-//                 cpf.request('["digitalWrite", 4 , 0]');
-//             }
-
-//             time = 0;
-//             x = 3;
+//     if (water == 0 && time > 5) {  //  紅燈   WaterSensor == 0 有水
+//         console.log(time);
+//         if (x != 1) {          //  LCD板的變數
+//             cpf.request('["grove_rgblcd_clear"]');
 //         }
 
-//         document.getElementById("lightValue").innerHTML = value[x - 1];
-//         document.getElementById("word").innerHTML = word[x - 1];
+//         cpf.request('["grove_setColorRGB", 0, 255, 0, 0]');
+//         cpf.request('["grove_rgblcd_print", 2, 0,"WaterClean"]');
+//         cpf.request('["grove_rgblcd_print", 4, 1,"NO ENTRY."]');
+//         cpf.request('["grove_rgblcd_set_rgb", 255, 160, 122]');
 
+//         if (x == 1) {
+//             cpf.request('["digitalWrite", 4 , 1]');
+//         }
+
+//         if (((time - 5) % 3) == 1) {
+//             cpf.SetSpeech("On", "cmn-Hant-TW", "現在地下道無法通行請駕駛改道", 0.4, 0.7);
+//         }
+//         time += 1;
+//         x = 1;
 //     }
-//     setTimeout(loop, 1000);
+//     else if (water == 0 && time <= 5) {   //  黃燈
+//         if (x != 2) {
+//             cpf.request('["grove_rgblcd_clear"]');
+//         }
+//         cpf.request('["digitalWrite", 4 , 0]');
+//         cpf.request('["grove_setColorRGB", 0, 255, 255, 0]');
+//         cpf.request('["grove_rgblcd_print", 2, 0,"WaterClean"]');
+//         cpf.request('["grove_rgblcd_print", 0, 1,"Drive carefully."]');
+//         cpf.request('["grove_rgblcd_set_rgb", 238, 238, 0]');
+//         time += 1;
+//         x = 2;
+//     }
+//     else if (water == 1) {    //  綠燈    WaterSensor == 1 沒水
+//         console.log('1  ' + time);
+
+//         if (x != 3) {
+//             cpf.request('["grove_rgblcd_clear"]');
+//         }
+
+//         cpf.request('["grove_setColorRGB", 0, 0, 255, 0]');
+//         //cpf.request('["grove_rgblcd_print", 2, 0,"Undergrand"]');
+//         //cpf.request('["grove_rgblcd_print", 2, 1,"Road safely."]');
+//         //cpf.request('["grove_rgblcd_set_rgb", 124, 252, 0]');
+
+//         console.log('2  ' + time);
+
+//         if (x == 3) {
+//             cpf.request('["digitalWrite", 4 , 0]');
+//         }
+
+//         time = 0;
+//         x = 3;
+//     }
+
+//     document.getElementById("lightValue").innerHTML = value[x - 1];
+//     document.getElementById("word").innerHTML = word[x - 1];
+
+// }
+// setTimeout(loop, 1000);
 // }
 // loop();
 
