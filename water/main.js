e3fc4821ce92933f1638b11a48629b55ad68a68a
JoTzu <ra92392@gmail.com>
2018-06-26 19:39:49 +0800

    new

--- water/main.js
+++ water/main.js
@@ -19,13 +19,13 @@ function loop() {
             //紅燈
             cpf.request('["grove_setColorRGB", 0, 255, 0, 0]');
             cpf.request('["grove_rgblcd_print", 2, 1,"Water"]');
-            cpf.request('["digitalWrite", 2 , 1]');
+            // cpf.request('["digitalWrite", 2 , 1]');
         }
         else {
             //綠燈
             cpf.request('["grove_setColorRGB", 0, 0, 255, 0]');
             cpf.request('["grove_rgblcd_print", 2, 1,"Clean"]');
-            cpf.request('["digitalWrite", 2 , 0]');
+            // cpf.request('["digitalWrite", 2 , 0]');
         }
         // console.log(water, time);
         // if (water == 0 && time > 5) {  //  紅燈   WaterSensor == 0 有水
@@ -94,6 +94,6 @@ loop();
 function setup() {
     if (cpf) {
         // cpf.setPinMode('["resetPin"], ["grove_rgblcd_begin", 16, 2], ["grove_newChainableLED",7, 8, 1], ["setPinMode", "digital", 4, "OUTPUT"], ["setPinMode", "digital", 6, "INPUT"]');
-        cpf.setPinMode('["resetPin"], ["grove_rgblcd_begin", 16, 2], ["grove_newChainableLED",7, 8, 1],["setPinMode", "digital", 6, "INPUT"],["setPinMode", "digital", 2, "OUTPUT"],');
+        cpf.setPinMode('["resetPin"],["grove_rgblcd_begin", 16, 2],["grove_newChainableLED",7, 8, 1],["setPinMode", "digital", 6, "INPUT"],["setPinMode", "digital", 2, "OUTPUT"],');
     }
 }
